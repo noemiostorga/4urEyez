869ce66b5e6c50a5e2ed0193944ebad979cba37b
noemiostorga <noemiostorga@gmail.com>
2017-05-22 18:29:07 -0700

    on click ouput changes and user input is captured

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
 function Capture(){
 	var guessInput = document.getElementById("UserInput").value;
 	console.log(guessInput);
-	document.getElementById("")
-}
-
-
-
-function Change(){
-document.getElementById("testing").innerHTML = "hey";
+	document.getElementById("ComplimentOutput").innerHTML = "changes to text that is assigned to this number";
 
 }
+
