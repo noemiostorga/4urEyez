b9a5da1141a8684b148d18242549db32b88b167f
noemiostorga <noemiostorga@gmail.com>
2017-05-22 18:09:05 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+function Capture(){
+	var guessInput = document.getElementById("UserInput").value;
+	console.log(guessInput);
+	document.getElementById("")
+}
+
+
+
+function Change(){
+document.getElementById("testing").innerHTML = "hey";
+
+}
